2ea4c209942393b826dd8b4aad3f44daf4729dea
marouanibourki7 <ibourki.anergui@gmail.com>
2023-03-16 17:37:07 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.>
+
+/**
+ * main -entry point
+ * description : AC program that prints with put function
+ *
+ * return : always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
