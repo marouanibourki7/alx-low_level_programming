f2365edb1a270be6f45d3051c97eee28703e2405
marouanibourki7 <ibourki.anergui@gmail.com>
2023-03-20 07:25:35 -0700

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : print all aplhabet letters
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+	char ch = 'a';
+
+	while (ch <= 'Z')
+	{
+		putchar(char);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
