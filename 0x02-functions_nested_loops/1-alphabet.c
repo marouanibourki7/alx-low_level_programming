88ac0d4595f5d92f0d8b68f3102770498e8bd501
marouanibourki7 <ibourki.anergui@gmail.com>
2023-03-22 06:43:41 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
 #include "main.h"
+
 /**
-* main - Entry Point
-*
-* print_alphabet -  prints the alphabet in lowercase
-*
-* Return: Always 0 (Success)
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
 */
 
 void print_alphabet(void)
