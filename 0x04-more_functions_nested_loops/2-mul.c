ac940cf4271f79fb206401950a136240c68e7f4c
marouanibourki7 <ibourki.anergui@gmail.com>
2023-03-23 15:07:46 -0700

    a

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * nul - mutliplies two integer
+ * mul - mutliplies two integer
  * @x: the first number to multiply
  * @y: the second number to multiply
  * Return: result of multiplication of two numbers
  */
-int nul(int x, int y)
+int mul(int x, int y)
 {
 	return (x * y);
 }
